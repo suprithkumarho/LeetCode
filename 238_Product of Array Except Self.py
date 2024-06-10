2ad7531d37a3ffdc9da856d400c57745e87ba7d5
suprithkumarho <kumar.suprithho@gmail.com>
2024-06-10 10:44:17 -0500

    Daily Commit 1

---
+++ 238_Product of Array Except Self.py
@@ -0,0 +1,45 @@
+'''
+Product of Array Except Self
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+'''
+
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        res = [1] *(len(nums))
+
+        leftval = 1
+        for i in range(len(nums)):
+            res[i] = leftval
+            leftval *= nums[i] 
+        rightval = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= rightval
+            rightval *= nums[i] 
+        return(res)
+        
+
+sol = Solution()
+print(sol.productExceptSelf([1,2,3,4]))
+
